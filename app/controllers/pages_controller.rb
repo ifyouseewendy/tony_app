df72793d2ecef3e71194fd4cad92a0810c9ff1a1
wendi <ifyouseewendy@gmail.com>
2016-03-26 20:46:40 +0800

    page: Update index for signed in user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,13 @@
 class PagesController < ApplicationController
+  def index
+    if current_user
+      @data = {
+        user_name: current_user.name,
+        sign_in_count: current_user.sign_in_count,
+        sign_in_minute: ( (Time.now - current_user.current_sign_in_at) / 60 ).to_i
+      }
+      render :index
+    else
+    end
+  end
 end
