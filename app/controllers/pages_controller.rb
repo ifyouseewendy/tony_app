f45376b706c082c2b9c82c67b43c1a8915dd4e41
wendi <ifyouseewendy@gmail.com>
2016-03-26 21:08:54 +0800

    page: Add stranger workflow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,33 @@
 class PagesController < ApplicationController
   def index
-    if current_user
+    if current_user.present?
       @data = {
+        user_signed_in: true,
         user_name: current_user.name,
         sign_in_count: current_user.sign_in_count,
-        sign_in_minute: ( (Time.now - current_user.current_sign_in_at) / 60 ).to_i
+        sign_in_time: ( (Time.now - current_user.current_sign_in_at) / 60 ).to_i
       }
       render :index
     else
+      if session[:stranger_id].present?
+        token = session[:stranger_id]
+        visit_time = ( (Time.now - cache.fetch(token)) / 60 ).to_i
+      else
+        token = Devise.friendly_token
+        session[:stranger_id] = token
+        cache.write(token, Time.now, expires_in: 1.minute)
+        visit_time = 0
+      end
+
+      @data = {
+        visit_time: visit_time
+      }
     end
   end
+
+  private
+
+    def cache
+      @_cache ||= Rails.cache
+    end
 end
